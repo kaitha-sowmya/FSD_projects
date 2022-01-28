d58fd777fcf9cc20877e239711f41b229c58ad75
sowmy <sowmy@LAPTOP-96QG03L3>
2022-01-28 16:48:58 +0530

    Assisted5 commit

---
+++ phase2/AssistedProjects/FilterDemo/src/main/java/LoginFilter.java
@@ -0,0 +1,56 @@
+
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		String userId = request.getParameter("userid");
+		//String userName = request.getParameter("username");
+        if( userId !=null){
+                chain.doFilter(request, response);
+         }
+        else {
+        	response.getWriter().write("check");
+        }
+         
+        
+
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
