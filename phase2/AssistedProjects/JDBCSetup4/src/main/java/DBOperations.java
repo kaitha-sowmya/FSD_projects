2c1605ea2b5262c3c420b3a7f3b077dd83bf1c23
sowmy <sowmy@LAPTOP-96QG03L3>
2022-01-29 13:36:44 +0530

    assisted2 4 commit

---
+++ phase2/AssistedProjects/JDBCSetup4/src/main/java/DBOperations.java
@@ -0,0 +1,74 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ecommerce.DBConnection;
+
+/**
+ * Servlet implementation class DBOperations
+ */
+public class DBOperations extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public DBOperations() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            
+           
+           
+           
+           DBConnection conn = new DBConnection();
+           Statement stmt = conn.DBConn().createStatement();
+           stmt.executeUpdate("create database Testing");
+           out.println("Created database: Testing<br>");
+           stmt.executeUpdate("use Testing");
+           out.println("Selected database: Testing<br>");
+           stmt.executeUpdate("drop database Testing");
+           stmt.close();
+           out.println("Dropped database: Testing<br>");
+           
+           
+           
+           
+          
+           
+           
+           out.println("</body></html>");
+         
+           
+   } catch (ClassNotFoundException e) {
+           e.printStackTrace();
+   } catch (SQLException e) {
+           e.printStackTrace();
+   }
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
